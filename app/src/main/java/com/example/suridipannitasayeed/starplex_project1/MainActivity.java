898995fe17bc65566a2c4ee1c358e5dbd3d1cb52
Suri Dipannita Sayeed <suridipannitasayeed@Shers-MacBook-Pro.local>
2016-03-21 16:19:37 +0600

    updated

--- app/src/main/java/com/example/suridipannitasayeed/starplex_project1/MainActivity.java
+++ app/src/main/java/com/example/suridipannitasayeed/starplex_project1/MainActivity.java
@@ -10,22 +10,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Toolbar tool;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.content_main);
+        tool=(Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(tool);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
